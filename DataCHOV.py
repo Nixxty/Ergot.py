cb92ee682469e8beeb0801f6b3209f671efdd1df
Nixxty <130720084+Nixxty@users.noreply.github.com>
2023-08-29 06:51:18 -0500

    Updated DataCHOV.py

--- DataCHOV.py
+++ DataCHOV.py
@@ -1,5 +1,11 @@
 import json, os
+#Data checker/Overwriter
+
+#vars
 mid = 'undefined'
+dir_p = os.path.dirname(os.path.realpath(__file__))
+dir_p = os.path.join(dir_p, 'UidData.json')
+uidfound = False
 def DataOV(mib):
     if mib == True:
         uinput = input("One of your IDS was undefined, check UidData.json and replace any ID missing/not defined (N/A) with the proper data. Optionally you can choose to overwrite the data from here. Overwrite data? (Y/N): ")
@@ -13,43 +19,56 @@ def DataOV(mib):
             tcid = input('channel_id = ')
             uinput = input("is " + tcid + " your preferred guild/server's channel id? (Y/N): ")
             if uinput.lower() == 'yes' or uinput.lower() == 'y':
-                taid = input('auth_id = ')
-                uinput = input("is " + taid + " your discord user id? (Y/N): ")
-                if uinput.lower() == 'yes' or uinput.lower() == 'y':
-                    dictionary = {
-                        "permissions": [
-                            {
-                        "guild_id": tgid,
-                        "channel_id": tcid,
-                        "auth_id": taid
-                            }
-                        ]
-                    }
-                    json_obj = json.dumps(dictionary, indent=3)
-                    with open("UidData.json", "w") as g:
-                        g.write(json_obj)
-                    g.close()
-                    print("Data overwritten.")
-                    input("CMD will close after you press enter, relaunch the file if you ever need/want to change it and or want to check your data.")
+                    btid = input('bot_token_id = ')
+                    uinput = input("is "+btid+" your bot token id? (Y/N): ")
+                    if uinput.lower() == 'yes' or uinput.lower() == 'y':
+                        btob = input('would you like ur bot token to be obscured when running Main? (Bool type, input either true or false ..): ')
+                        if btob == 'true':
+                            btob = True
+                        elif btob == 'false':
+                            btob = False
+                        uinput = input("is "+str(btob)+" your bool input? (Y/N): ")
+                        if uinput() == 'yes' or uinput.lower() == 'y':
+                            dictionary = {
+                            "permissions": [
+                                {
+                            "guild_id": tgid,
+                            "channel_id": tcid,
+                            "token_id": btid,
+                            "token_ob": btob
+                                }
+                            ]
+                        }
+                        json_obj = json.dumps(dictionary, indent=4)
+                        with open(dir_p, "w") as g:
+                            g.write(json_obj)
+                        g.close()
+                        print("Data overwritten.")
+                        input("CMD will close after you press enter, relaunch the file if you ever need/want to change it or want to check your data.")
+                        exit(0)
 def init():
+    #try opening Uid and checking if the file exists.
     try:
-        f = open('UidData.json')
+        f = open(dir_p)
         f.close
         print("UidData.json was found!")
-        main()
+        global uidfound
+        uidfound = True
     except FileNotFoundError:
+        #once file was confirmed to not exist create the file and add undefined permissions
         print("File was not found! creating file")
         dictionary = {
             "permissions": [
                 {
                 "guild_id": mid,
                 "channel_id": mid,
-                "auth_id": mid
+                "token_id": mid,
+                "token_ob": mid
                 }
             ]
         }
-        json_obj = json.dumps(dictionary, indent=3)
-        with open("UidData.json", "w") as g:
+        json_obj = json.dumps(dictionary, indent=4)
+        with open(dir_p, "w") as g:
            g.write(json_obj)
            g.close()
            input("relaunch this file!")
@@ -57,20 +76,24 @@ def init():
 
 
 def main():
-    with open('UidData.json', 'r+') as f:
+    with open(dir_p, 'r+') as f:
         data = json.load(f)
         f.close()
         for i in data['permissions']:
             guild_id = i['guild_id']
             channel_id = i['channel_id']
-            auth_id = i['auth_id']
+            token_id = i['token_id']
+            bot_token_obscure = i['token_ob']
             mib = False
-            if guild_id==mid or channel_id==mid or auth_id==mid:
+            if guild_id==mid or channel_id==mid or token_id==mid or bot_token_obscure==mid:
                 mib = True
                 DataOV(mib)
-            elif guild_id!=mid or channel_id!=mid or auth_id!=mid:
-                print("Guild ID: " + guild_id + "Channel ID: " + channel_id + "Auth_ID: " + auth_id)
+
+            elif guild_id!=mid or channel_id!=mid or token_id!=mid or bot_token_obscure!=mid:
+                if bot_token_obscure == True:
+                    print("Guild ID: " + guild_id + "\nChannel ID: " + channel_id + "\nBot_Token: OBSCURED.")
+                else:
+                    print("Guild ID: " + guild_id + "\nChannel ID: " + channel_id + "\nBot_Token: " + token_id)
                 uinput = input("Do you wish to overwrite your data? (Y/N): ")
                 if uinput.lower() == 'yes' or uinput.lower() == 'y':
                     DataOV(mib)
-init()
