4b8b9a2ed094dce8b7c9bb3dd3809c43dbc3e038
Nixxty <130720084+Nixxty@users.noreply.github.com>
2023-08-29 07:37:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from updbundle import updc
 import dependencies
 updc.maincheck()
 dependencies.dep()
-uinput = input("\nRun bot = 1\nrun Data checker/overwriter = 2\noptions: 1 | 2\n")
+uinput = input("\nRun bot = 1\nrun Data checker/overwriter = 2\nNOTE: If its your first time running main.py or datachov.py use option 2\noptions: 1 | 2\n")
 if uinput == '1':
     print('running bot.')
     from mainbundle import bot
