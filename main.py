278e1ffdebf4206fd2c4660141c4858511b77e9d
Nixxty <130720084+Nixxty@users.noreply.github.com>
2023-08-31 05:16:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from updbundle import updc
 import dependencies
 updc.maincheck()
 dependencies.dep()
-uinput = input("\nRun bot = 1\nrun Data checker/overwriter = 2\nNOTE: If its your first time running main.py or datachov.py use option 2\noptions: 1 | 2\n")
-if uinput == '1':
-    print('running bot.')
-    from mainbundle import bot
-elif uinput == '2':
-    DataCHOV.init()
-    if DataCHOV.uidfound == True:
-        DataCHOV.main()
+def mainop():
+    uinput = input("\nRun bot = 1\nrun Data checker/overwriter = 2\noptions: 1 | 2\n")
+    if uinput == '1':
+        print('running bot.')
+        from mainbundle import bot
+    elif uinput == '2':
+        DataCHOV.init()
+        if DataCHOV.uidfound == True:
+            DataCHOV.main()
+            if DataCHOV.returned == False:
+                mainop()
+mainop()
