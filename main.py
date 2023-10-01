51e686bc8684e4d5cf52ac58b1f84c12c3b45832
Nixxty <130720084+Nixxty@users.noreply.github.com>
2023-10-01 06:01:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 #main file, runs everything from this single file including datachov and bot file.
-
 from mainbundle import DataCHOV
-from updbundle import updc
-import dependencies
+from updbundle import setup, updc
+from mainbundle.exts.cmds import clearterminal
+args = ['1','2'] #arguments for if statements lower in the code, was used in hopes of making the code more readable.
+
+setup.setup()
+clearterminal(1,3)
 updc.maincheck()
-dependencies.dep()
-def mainop():
-    uinput = input("\nRun bot = 1\nrun Data checker/overwriter = 2\noptions: 1 | 2\n")
-    if uinput == '1':
-        print('running bot.')
+clearterminal(1,3)
+## if passed check
+
+def Selector():
+    clearterminal(0,2)
+    UserInput = input("Run bot = 1\nrun Data checker/overwriter = 2\noptions: 1 | 2\n\n")
+
+    if UserInput == args[0]: #BOTRUN
+        clearterminal(0,0)
         from mainbundle import bot
-    elif uinput == '2':
+
+    elif UserInput == args[1]: #DATACHOV RUN
+
         DataCHOV.init()
-        if DataCHOV.uidfound == True:
+        if DataCHOV.userIDfound == True:
+            clearterminal(0,1)
             DataCHOV.main()
             if DataCHOV.returned == False:
-                mainop()
-mainop()
+                Selector()
+
+    if UserInput != args: #RESTART SELECTOR IF NOT EQUAL TO CORRECT ARG
+        Selector()
+Selector()
